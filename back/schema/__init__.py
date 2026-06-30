9b1d09037b37e9f4a02185f8bdcea0d17e4ecb1f
nicolas <nikitopedroza2013@gmail.com>
2026-06-30 00:07:08 -0300

    schema para los comentarios

--- back/schema/__init__.py
+++ back/schema/__init__.py
@@ -1,3 +1,4 @@
 from schema.users import UserCreate, CheckDuplicado, AllUsersResponse, PaginatedUsers
 from schema.social_media import SocialMediaCreate
 from schema.infidelity import InfidelityEvidenciaCreate, InfidelityCreate
+from schema.comments import CommentCreate
--- back/schema/comments.py
+++ back/schema/comments.py
@@ -1,1 +1,6 @@
-#HACER
+from pydantic import BaseModel
+from uuid import UUID
+
+class CommentCreate(BaseModel):
+    parent_id: UUID | None = None
+    mensaje: str
